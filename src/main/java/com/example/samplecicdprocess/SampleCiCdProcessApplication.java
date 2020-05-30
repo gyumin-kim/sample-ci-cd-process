0aaeed32fa527a46b907202c24b39010a8d3dd23
Gyumin Kim <starkying@gmail.com>
2020-05-30 11:29:37 +0900

    Initialize application

---
+++ src/main/java/com/example/samplecicdprocess/SampleCiCdProcessApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplecicdprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleCiCdProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleCiCdProcessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/samplecicdprocess/SampleCiCdProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplecicdprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleCiCdProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
